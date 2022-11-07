eb2393de0b316d25ab6640999177b5fe15e90a7a
Nanasiblue <93894196+Nanasiblue@users.noreply.github.com>
2022-11-07 17:09:08 +0900

    Finish kadai1

---
+++ prmn2022/src/lecture02/Exercise2_1.java
@@ -0,0 +1,10 @@
+package lecture02;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+        Human human1 = new Human("keita", 20);
+        Human human2 = new Human("Hirosi", 35);
+        human1.print();
+        human2.print();
+    }
+}
--- prmn2022/src/lecture02/Human.java
+++ prmn2022/src/lecture02/Human.java
@@ -1,7 +1,24 @@
 package lecture02;
 
 public class Human {
-    public static void main(String[] args) {
 
+    String name;
+    int age;
+
+    Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void print( ) {
+        if (age >= 23) {
+            System.out.println(name + " " + age + "歳です");
+        }
+        if (age < 18) {
+            System.out.println("生徒:" + name + " " + age + "歳です");
+        }
+        if (age >= 18 && age < 22) {
+            System.out.println("学生:" + name + " " + age + "歳です");
+        }
     }
 }
