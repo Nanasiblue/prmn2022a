1ebd4e87646548077b3cbe2db984408c32cd3e8a
Nanasiblue <93894196+Nanasiblue@users.noreply.github.com>
2022-11-20 16:43:02 +0900

    4_1

--- prmn2022/src/lecture04/Exercise4_1.java
+++ prmn2022/src/lecture04/Exercise4_1.java
@@ -7,12 +7,12 @@ public class Exercise4_1 {
         while ( true ) {
             fighter1.attack( fighter2.hitPoint );
             if ( !fighter2.isAlive() ) {
-                System.out.println( fighter2.name + " は倒れた！" );
+                System.out.println( fighter2.name + " は倒れた！" );//falseならHP<0　倒れたことを表示
                 System.exit( 0 );
             }
             fighter2.attack( fighter1.hitPoint );
             if ( !fighter1.isAlive() ) {
-                System.out.println( fighter1.name + " は倒れた！" );
+                System.out.println( fighter1.name + " は倒れた！" );//falseならHP<0　倒れたことを表示
                 System.exit( 0 );
             }
         }
--- prmn2022/src/lecture04/Fighter.java
+++ prmn2022/src/lecture04/Fighter.java
@@ -15,14 +15,14 @@ public class Fighter {
         hitPoint = hitPoint - power;
         System.out.println( name + " は " + name + " に " + power + " ダメージを与えた。" );
         if ( hitPoint >= 0 ){
-            System.out.println( name + "　の残りHP　: " + hitPoint );
+            System.out.println( name + "　の残りHP　: " + hitPoint );//HP>0ならそのまま表示
         }
-        else System.out.println( name + "　の残りHP　: " + 0 );
+        else System.out.println( name + "　の残りHP　: " + 0 );//HP<0なら0で表示
     }
 
     boolean isAlive( ) {
         boolean b;
-        b = ( hitPoint > 0 );
+        b = ( hitPoint > 0 );//HP>0ならtrue
         return b;
         }
     }
