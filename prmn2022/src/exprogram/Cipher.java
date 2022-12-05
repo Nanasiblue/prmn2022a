ecf8502e48a68de66ff1ab3708cd43eaa072f15c
Nanasiblue <93894196+Nanasiblue@users.noreply.github.com>
2022-12-05 18:06:20 +0900

    Cipher

--- prmn2022/src/exprogram/Cipher.java
+++ prmn2022/src/exprogram/Cipher.java
@@ -40,7 +40,7 @@ public class Cipher {
             }
         }
         for ( int i = 0; i < ptxt.length(); i++ ) {
-            for ( int j = 0; i < japanese.length(); j++ ) {
+            for ( int j = 0; j < japanese.length(); j++ ) {
                 if ( ptxt.charAt(i) == japanese.charAt(j) ) {//一文字ずつ取り出して確認し、入力とひらがなが一致したときに乱数で決めた中から選んで暗号化
                     int r = rand.nextInt( 1000 );
                     Enc.add(ramdom[j][r]);
@@ -66,11 +66,12 @@ public class Cipher {
                 number++;
             }
         }
-        String[] dtxt = ctxt.split(",");//入力された文字列を分割する
+        String[] dtxt;
+        dtxt = ctxt.split(",");//入力された文字列を分割する
         for ( int i = 0; i < dtxt.length; i++ ) {//暗号化と逆の手順で一文字ずつ数字を文字に置き換える
             for ( int j = 0; j < 100; j++ ) {
                 for ( int k = 0; k < 1000; k++ ) {
-                    if ( Integer.parseInt(dtxt[j]) == ramdom[j][k] ) {//intにキャストして比較する
+                    if ( Integer.parseInt(dtxt[i]) == ramdom[j][k] ) {//intにキャストして比較する
                         Dec.add(japanese.charAt(j));
                     }
                 }
